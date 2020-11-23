5aa1ea10d0e988f615ed3a1247aa59866ffacc5e
schckngs <andreahilborn@gmail.com>
2020-11-23 11:11:47 -0800

    Test if matrix or RasterLayer input

--- R/fill_gaps.R
+++ R/fill_gaps.R
@@ -8,22 +8,53 @@
 #' @return Filled RasterLayer
 #' @export
 sparkle_fill <- function(x, min_sides, fun, ...) {
-    require(dplyr)
-    idx_na <- which(as.vector(is.na(x)) == TRUE)
-    adj <- adjacent(x, idx_na, directions= 8, include = F, id=T)
-    adj <- as.data.frame(adj)
-    adj$val <- x[adj$to]
-    adj <- adj %>% group_by(id) %>% mutate(n = sum(!is.na(val))) %>% ungroup() %>%
-        filter(n >= (8-min_sides))
-    if (fun == "mean") {
-        adj <- adj %>% group_by(id) %>% summarise(fillval = mean(val, na.rm = T)) %>% ungroup()
-        x[idx_na[adj$id]] <- adj$fillval
-    } else if (fun == "median") {
-        adj <- adj %>% group_by(id) %>% summarise(fillval = median(val, na.rm = T)) %>% ungroup()
-        x[idx_na[adj$id]] <- adj$fillval
-    } else if (fun == "bilinear") {
-        x2 <- raster::resample(x, x, "bilinear")
-        x[idx_na[adj$id]] <- x2[idx_na[adj$id]]
+    if (class(x)[1] == "RasterLayer") {
+        require(dplyr)
+        idx_na <- which(as.vector(is.na(x)) == TRUE)
+        adj <- raster::adjacent(x, idx_na, directions= 8, include = F, id=T)
+        adj <- as.data.frame(adj)
+        adj$val <- x[adj$to]
+        adj <- adj %>% group_by(id) %>% mutate(n = sum(!is.na(val))) %>% ungroup() %>%
+            filter(n >= (8-min_sides))
+        if (fun == "mean") {
+            adj <- adj %>% group_by(id) %>% summarise(fillval = mean(val, na.rm = T)) %>% ungroup()
+            x[idx_na[adj$id]] <- adj$fillval
+        } else if (fun == "median") {
+            adj <- adj %>% group_by(id) %>% summarise(fillval = median(val, na.rm = T)) %>% ungroup()
+            x[idx_na[adj$id]] <- adj$fillval
+        } else if (fun == "bilinear") {
+            x2 <- raster::resample(x, x, "bilinear")
+            x[idx_na[adj$id]] <- x2[idx_na[adj$id]]
+        }
+        return(x)
+    } else if (is.matrix(x) == TRUE) {
+        if (missing(xlon) | missing(xlat)) {
+            print("Please enter xlat and xlon matrices")
+        } else {
+            print("exist")
+            require(dplyr)
+            x <- raster::raster(x, xmn = min(xlon), xmx = max(xlon),
+                                 ymn = min(xlat), ymx = max(xlat))
+            idx_na <- which(as.vector(is.na(x)) == TRUE)
+            adj <- raster::adjacent(x, idx_na, directions= 8, include = F, id=T)
+            adj <- as.data.frame(adj)
+            adj$val <- x[adj$to]
+            adj <- adj %>% group_by(id) %>% mutate(n = sum(!is.na(val))) %>% ungroup() %>%
+                filter(n >= (8-min_sides))
+            if (fun == "mean") {
+                adj <- adj %>% group_by(id) %>% summarise(fillval = mean(val, na.rm = T)) %>% ungroup()
+                x[idx_na[adj$id]] <- adj$fillval
+            } else if (fun == "median") {
+                adj <- adj %>% group_by(id) %>% summarise(fillval = median(val, na.rm = T)) %>% ungroup()
+                x[idx_na[adj$id]] <- adj$fillval
+            } else if (fun == "bilinear") {
+                x2 <- raster::resample(x, x, "bilinear")
+                x[idx_na[adj$id]] <- x2[idx_na[adj$id]]
+            }
+            x <- as.matrix(x)
+            return(x)
+        }
+    } else {
+        print("Data type is not one of matrix or RasterLayer")
     }
-    return(x)
 }
