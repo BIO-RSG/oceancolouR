10a45bd5c5a5b35d6fe66bafc1a6e8ccc65937c4
Clay <Stephanie.Clay@dfo-mpo.gc.ca>
2021-01-06 19:37:44 -0400

    Remove unnecessary function get_dir

--- R/formatting.R
+++ R/formatting.R
@@ -1,16 +1,3 @@
-#' Create folder
-#'
-#' Check for existence of a folder, and create it if necessary.
-#'
-#' @param path Path to create (a string). Absolute or relative.
-#' @return The path name.
-#' @export
-get_dir <- function(path) {
-    if (!dir.exists(path)) {dir.create(path, showWarnings=F)}
-    path
-}
-
-
 # Source: https://stackoverflow.com/questions/24956546/capitalizing-letters-r-equivalent-of-excel-proper-function
 #' Capitalization
 #'
