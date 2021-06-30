e6441cef0c83090453d584e2ce73872c3ab49a1c
Clay <Stephanie.Clay@dfo-mpo.gc.ca>
2021-06-30 08:56:17 -0300

    Add example to ocx() documentation and add "global" option to coefficient list in get_ocx_coefs()

--- R/OCX.R
+++ R/OCX.R
@@ -4,20 +4,22 @@
 #' "ocx" returns the coefficients of the global ocean colour algorithm used by NASA (as of Nov 2020) for the selected sensor. "poly1" to "poly4" return the regionally-optimized coefficients for polynomial algorithms of degrees 1 to 4 for the selected sensor, for the two available regions: nwa (Northwest Atlantic) or nep (Northeast Pacific).
 #'
 #' @param sensor String, either "modis", "seawifs", or "viirs" (note: "modis" is MODIS-Aqua, and "viirs" is VIIRS-SNPP)
-#' @param region String, either "nwa" or "nep"
+#' @param region String, either "global" (for ocx), or "nwa" or "nep" (for ocx or poly1 to poly4)
 #' @param alg String, either "poly1", "poly2", "poly3", "poly4", or "ocx"
 #' @references
 #' Clay, S.; Peña, A.; DeTracey, B.; Devred, E. Evaluation of Satellite-Based Algorithms to Retrieve Chlorophyll-a Concentration in the Canadian Atlantic and Pacific Oceans. Remote Sens. 2019, 11, 2609.
 #' https://www.mdpi.com/2072-4292/11/22/2609
 #' @return Numeric vector of polynomial coefficients, from the coefficient on the lowest degree term to highest degree.
 #' @export
-get_ocx_coefs <- function(sensor, region, alg) {
+get_ocx_coefs <- function(sensor, region="global", alg="ocx") {
 
     stopifnot(sensor %in% c("modis", "seawifs", "viirs"),
-              region %in% c("nwa", "nep"),
-              alg %in% c("poly1", "poly2", "poly3", "poly4", "ocx"))
+              ((region=="global" & alg=="ocx") | (region %in% c("nwa", "nep") & alg %in% c("poly1", "poly2", "poly3", "poly4", "ocx"))))
 
-    coefs <- list("nwa"=list("modis"=list("poly1"=c(0.36695,-3.27757),
+    coefs <- list("global"=list("modis"=list("ocx"=c(0.2424,-2.7423,1.8017,0.0015,-1.228)),
+                                "seawifs"=list("ocx"=c(0.3272,-2.994,2.7218,-1.2259,-0.5683)),
+                                "viirs"=list("ocx"=c(0.2228,-2.4683,1.5867,-0.4275,-0.7768))),
+                  "nwa"=list("modis"=list("poly1"=c(0.36695,-3.27757),
                                           "poly2"=c(0.37539,-3.12409,-0.75408),
                                           "poly3"=c(0.37657,-3.26173,-0.60435,1.1404),
                                           "poly4"=c(0.37925,-3.28487,-0.7583,1.49122,0.8002),
@@ -176,6 +178,33 @@ get_br <- function(rrs, blues, green, use_443nm=FALSE) {
 #' https://oceancolor.gsfc.nasa.gov/atbd/chlor_a/
 #'
 #' @return For matrix rrs: Numeric value (or vector) -- chlorophyll as computed by OCX for the given Rrs, sensor, and coefficients. For RasterStack rrs: equivalent raster with OCx chlorophyll-a.
+#' @examples
+#' # Some in situ chl / MODIS Rrs data used in Clay et al (2019)
+#' input <- matrix(c(0.118, 0.0072, 0.0064, 0.0035, 0.122, 0.0048, 0.005, 0.0017, 0.128, 0.0076, 0.007, 0.0032, 0.198, 0.0072, 0.007, 0.0035, 0.199, 0.0137, 0.0099, 0.005, 0.206, 0.0049, 0.005, 0.0027, 0.208, 0.0083, 0.0074, 0.0035, 0.213, 0.0035, 0.0036, 0.0023, 0.215, 0.0053, 0.0057, 0.0032, 0.217, 0.0031, 0.0041, 0.0026, 0.22, 0.0067, 0.0066, 0.0034, 0.223, 0.0032, 0.0035, 0.0023, 0.223, 0.0042, 0.0045, 0.0024, 0.249, 0.0185, 0.0125, 0.0062, 0.249, 0.0027, 0.0056, 0.005, 0.254, 0.0048, 0.0055, 0.0035, 0.403, 0.0052, 0.0055, 0.0026, 0.404, 0.0054, 0.0054, 0.0043, 0.404, 0.0026, 0.003, 0.0023, 0.418, 0.004, 0.0042, 0.0028, 0.438, 0.0053, 0.0054, 0.0032, 0.438, 0.0047, 0.0048, 0.0034, 0.5, 0.0045, 0.0048, 0.0038, 0.501, 0.0047, 0.0074, 0.0069, 0.508, 0.0138, 0.0114, 0.0075, 0.511, 0.0047, 0.0053, 0.0037, 0.958, 0.0023, 0.0034, 0.003, 0.971, 0.0072, 0.0054, 0.0038, 1.253, 0.0019, 0.003, 0.0028, 1.253, 0.0108, 0.0058, 0.0034, 1.259, 0.0017, 0.0026, 0.0026, 1.261, 0.0057, 0.0073, 0.0074, 1.264, 0.0031, 0.0032, 0.0027, 1.269, 0.0033, 0.0044, 0.0044, 1.273, 0.0047, 0.0045, 0.0036, 1.311, 0.0043, 0.0046, 0.0031, 1.975, 0.0066, 0.0051, 0.0038, 1.975, 0.0067, 0.0065, 0.0043, 1.994, 0.0016, 0.0026, 0.0029, 1.999, 0.0022, 0.0037, 0.0033, 2.019, 0.0024, 0.0032, 0.0035, 2.551, 0.0059, 0.0043, 0.0024, 3.01, 0.0037, 0.0044, 0.0036, 3.035, 8e-04, 0.0026, 0.0031, 3.064, 0.0043, 0.0042, 0.0034, 3.086, 0.0077, 0.0081, 0.0072, 3.148, 0.0061, 0.0045, 0.0034, 3.216, 0.0027, 0.0034, 0.0035, 3.222, 0.0059, 0.0046, 0.0035, 4.47, 0.0033, 0.0042, 0.0033, 4.558, 0.0052, 0.0053, 0.0037, 4.575, 0.0051, 0.0042, 0.004, 4.613, 0.0031, 0.0034, 0.0034, 4.653, 0.0014, 0.0023, 0.0033, 4.749, 6e-04, 0.0019, 0.0034, 6.644, 0.0046, 0.0039, 0.0037, 6.825, 0.0015, 0.0023, 0.0026, 6.832, 0.0042, 0.0047, 0.0045, 6.954, 0.0053, 0.0045, 0.0034, 7.049, 0.0036, 0.0034, 0.0039, 7.099, 3e-04, 0.0013, 0.0026, 7.162, 0.0027, 0.0027, 0.003, 7.407, 0.0025, 0.003, 0.0035, 7.462, 0.0056, 0.0052, 0.0049, 7.79, 0.0012, 0.0019, 0.0028, 7.89, 0.0013, 0.0022, 0.0028, 8.142, 0.0044, 0.0044, 0.0047, 8.162, 5e-04, 0.0014, 0.0024, 8.869, 0.0011, 0.0022, 0.0029, 9.274, 0.0018, 0.0022, 0.0026, 9.533, 0.0015, 0.0022, 0.003), ncol=4, byrow=TRUE)
+#' colnames(input) <- c("in_situ_chl", "Rrs_443", "Rrs_488", "Rrs_547")
+#' rrs <- input[,2:4]
+#' chl <- input[,1]
+#'
+#' # get the default globally-tuned ocx coefficients for MODIS-Aqua
+#' best_alg_coefs <- get_ocx_coefs("modis", region="global", alg="ocx")
+#' # get the wavebands used in the ocx algorithm for modis, and include the 443nm band as an option
+#' use_443nm <- TRUE
+#' lambdas <- get_ocx_lambda("modis", use_443nm = use_443nm)
+#' # calculate the band ratio
+#' br <- get_br(rrs=rrs, blues=lambdas$blues, green=lambdas$green, use_443nm=use_443nm)$rrs_ocx
+#' # calculate ocx chl for each data point using those coefficients
+#' sat_ocx_chl <- ocx(rrs=rrs, blues=lambdas$blues, green=lambdas$green, coefs=best_alg_coefs)
+#'
+#' # plot in situ against satellite chl
+#' library(ggplot2)
+#' ggplot(data.frame(in_situ_chl=chl, sat_ocx_chl=sat_ocx_chl, stringsAsFactors=FALSE),
+#'        aes(x=in_situ_chl, y=sat_ocx_chl)) +
+#'     geom_point() +
+#'     geom_abline(slope=1, intercept=0) +
+#'     scale_x_log10(limits=c(0.1, 15)) +
+#'     scale_y_log10(limits=c(0.1, 15)) +
+#'     theme_bw()
+#'
 #' @export
 ocx <- function(rrs, blues, green, coefs, use_443nm=FALSE) {
 
