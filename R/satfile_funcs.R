ae6ca636a99b48d7c60c747e722501ea885b5c67
Clay <Stephanie.Clay@dfo-mpo.gc.ca>
2021-09-27 09:22:13 -0300

    - reorganize functions and add flag column to example satellite file for use in other function manuals

--- R/satfile_funcs.R
+++ R/satfile_funcs.R
@@ -23,3 +23,26 @@ read_h5_L3b <- function(h5_file, var_name) {
                 BinIndex = BinIndex))
 
 }
+
+
+#
+# # num = base 2 flag number to convert to binary string
+# get_flag_data <- function(num,dict) {
+#     dict[as.logical(intToBits(num)),]
+# }
+#
+# # given a matrix of quality flags, separate them into matrices for individual flags
+# # note we're using bit INDICES, not bit numbers (i.e. for bit 0, use which_bit_inds=1)
+# separate_flags <- function(flag_mat, which_bit_inds) {
+#     flag_bits <- lapply(as.numeric(flag_mat),intToBits)
+#     flag_bits <- matrix(do.call(rbind,flag_bits)[,which_bit_inds],nrow=length(flag_bits))
+#     flag_bits <- matrix(as.logical(flag_bits),nrow=nrow(flag_bits))
+#     flag_bits <- lapply(1:ncol(flag_bits),function(x) matrix(flag_bits[,x],nrow=nrow(flag_mat)))
+#     return(flag_bits)
+# }
+
+
+
+
+
+
