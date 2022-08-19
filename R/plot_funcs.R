6b92e1ac5f87519763b9db559e63435ccce30630
Clay <Stephanie.Clay@dfo-mpo.gc.ca>
2022-08-19 11:46:31 -0300

    - add option to adjust raster transparency in make_raster_map()

--- R/plot_funcs.R
+++ R/plot_funcs.R
@@ -45,6 +45,7 @@ sinh_trans <- function() {
 #' @param cm Color scale
 #' @param set_extremes TRUE/FALSE, should values outside the range in col_limits be set to the min/max? If not, they will be transparent. Ignored if col_limits=NULL
 #' @param na.value Color to use for NA values in raster
+#' @param rast_alpha Transparency of the raster on the map, 0-1
 #' @param map_alpha Transparency of landmasses on map, 0-1
 #' @param map_fill Colour of the landmasses in the map
 #' @param map_colour Colour of the outline of the landmasses in the map
@@ -65,7 +66,7 @@ sinh_trans <- function() {
 #' make_raster_map(log10(tr),title=NULL)
 #'
 #' @export
-make_raster_map <- function(rast,title=NULL,xlim=c(-95,-42),ylim=c(39,82),xlabs=NULL,ylabs=NULL,col_limits=NULL,cm=colorRampPalette(c("#00007F","blue","#007FFF","cyan","#7FFF7F","yellow","#FF7F00","red","#7F0000"))(100),set_extremes=FALSE,na.value="transparent",map_alpha=0.8,map_fill="white",map_colour="#7f7f7f",nrow=1,show_legend=TRUE) {
+make_raster_map <- function(rast,title=NULL,xlim=c(-95,-42),ylim=c(39,82),xlabs=NULL,ylabs=NULL,col_limits=NULL,cm=colorRampPalette(c("#00007F","blue","#007FFF","cyan","#7FFF7F","yellow","#FF7F00","red","#7F0000"))(100),set_extremes=FALSE,na.value="transparent",rast_alpha=1,map_alpha=0.8,map_fill="white",map_colour="#7f7f7f",nrow=1,show_legend=TRUE) {
     stopifnot(class(rast) %in% c("RasterStack","RasterLayer"))
     worldmap <- ggplot2::map_data("world")
     if (!is.null(col_limits)) {
@@ -85,7 +86,7 @@ make_raster_map <- function(rast,title=NULL,xlim=c(-95,-42),ylim=c(39,82),xlabs=
         colscale <- scale_fill_gradientn(colours = cm, na.value=na.value)
     }
     p <- rasterVis::gplot(rast) +
-        geom_tile(aes(fill = value), show.legend=show_legend) +
+        geom_tile(aes(fill = value), show.legend=show_legend, alpha=rast_alpha) +
         geom_map(data = worldmap, map = worldmap,
                  aes(x = long, y = lat, group = group, map_id=region),
                  fill = map_fill, colour = map_colour, size=0.5, alpha=map_alpha) +
